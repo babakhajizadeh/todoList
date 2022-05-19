fdbb1bade0d6eae8005c6c4359b46cf3f9fdd354
Babak Hajizadeh <babakhajizadeh@protonmail.com>
2022-05-19 15:51:27 +0430

    added to layout instead of static geometry.

--- addbutton.cpp
+++ addbutton.cpp
@@ -12,14 +12,14 @@ addButton::~addButton()
 void addButton::init()
 {
     add = new QPushButton;
-
-    this->add->setGeometry(370,30,70,50);
-    this->add->setText(" ADD ");
-    this->add->setStyleSheet("QPushButton {background-color: #A3C1DA}");
-
+    parentLayout = new QHBoxLayout(this);
+    parentLayout->addWidget(add);;
+    parentLayout->setSpacing(0);
+    add->setFixedHeight(50);
+    add->setText(" ADD ");
+    add->setStyleSheet("QPushButton {background-color: #A3C1DA}");
     qInfo() << "bottun geometry set.";
-    this->add->setParent(this);
-
+    add->setParent(this);
     connect (add,
              SIGNAL(released()),
              this,
